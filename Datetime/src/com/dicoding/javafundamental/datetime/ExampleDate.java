2076670e6eee379229db997c75ed3f449a8d856f
ayufebriani <73220887+ayufebriani@users.noreply.github.com>
2021-01-09 20:24:15 +0700

    Add files via upload

---
+++ Datetime/src/com/dicoding/javafundamental/datetime/ExampleDate.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dicoding.javafundamental.datetime;
+
+import java.util.Date;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ExampleDate {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println("Tanggal sekarang adalah " + date.toString());
+    }
+}
